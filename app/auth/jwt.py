672eda55d1d59409dad6165bb2effd0233e9dea0
Mithil Buddhima <mithilbuddhima@192.168.1.7>
2025-10-27 11:31:44 +0530

    Initial commit

---
+++ app/auth/jwt.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from typing import Union
+from app.models import User as UserModel
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise JWTError("Could not validate credentials")
+        user = UserModel(email=email) 
+        return user
+    except JWTError:
+        raise JWTError("Invalid token")
